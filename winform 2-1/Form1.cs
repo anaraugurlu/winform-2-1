f90ff255f8c3dc49adca7c8be8c2e217e485b255
Ugur_ml97 <Ugur_ml97@itstep.edu.az>
2021-06-20 21:28:46 +0400

    Добавьте файлы проекта.

---
+++ winform 2-1/Form1.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform_2_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            nameupdatetxtb.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            List<Human> humans = new List<Human>
+            {
+                new Human
+                {
+                    Email ="ZeynebHesenova@gmail.com",
+                    Name="zeyneb",
+                    Surname ="hesenova",
+                    PhoneNumber ="12312323535",
+
+                },
+
+                new Human
+                {
+Name ="fuad",
+Surname ="ugurlu",
+Email ="fuad123@gmail.com",
+PhoneNumber ="+12235256"
+                },
+                new Human
+                {
+                    Name ="nezrin",
+                    Surname ="rehimli",
+                    Email="nezrin123@gmail.com",
+                    PhoneNumber ="+9941312334"
+                }
+
+
+
+        };
+            foreach (var item in humans)
+            {
+
+                Humans.Items.Add(item);
+            }
+            Json.JsonSerialize(humans);
+        }
+
+        private void Addbtn_Click(object sender, EventArgs e)
+        {
+            Human human = new Human();
+            human.Name = textBox1.Text;
+            human.Surname = textBox2.Text;
+            human.Email = textBox4.Text;
+            human.BirthDate = dateTimePicker1.Text;
+            Humans.Items.Add(human);
+        }
+
+        private void UpdateBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                nameupdatetxtb.Enabled = true;
+
+                foreach (Human item in Humans.Items)
+                {
+
+                    if (item.Name == nameupdatetxtb.Text)
+                    {
+                        Humans.Items.Remove(item);
+                        Human human = new Human();
+                        human.Name = textBox1.Text;
+                        human.Surname = textBox2.Text;
+                        human.Email = textBox4.Text;
+                        human.BirthDate = dateTimePicker1.Text;
+                        Humans.Items.Add(human);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+
+            }
+                 
+                }
+            }
+            
+        }
+
+    
+
---
+++ winform 2-1/Json.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace winform_2_1
+{
+    class Json
+    {
+        static void JsonDeserializer()
+        {
+
+
+            //Human[] humans2 = null;
+            //var serializer = new JsonSerializer();
+            //using (StreamReader  sr = new StreamReader("humans.json"))
+            //{
+            //    using (var jw = new JsonTextWriter(sr))
+            //    {
+
+            //        humans2 = serializer.Deserialize(sr)
+                    
+            //            }
+            //} 
+
+        }
+     public   static void JsonSerialize(List<Human>humans)
+        {
+            
+            var serializer = new JsonSerializer();
+            using (var sw = new StreamWriter("humans.json"))
+
+
+            {
+
+
+                using (var jw = new JsonTextWriter(sw))
+
+
+                {
+
+
+                    jw.Formatting = Newtonsoft.Json.Formatting.Indented;
+
+
+                    serializer.Serialize(jw, humans);
+
+
+                }
+
+
+            }
+        }
+    }
+}
